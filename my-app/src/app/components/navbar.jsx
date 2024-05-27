24f0f998a914ec498507b2598d4d36ee9277b389
Daniel Hideo Matunoshita <dandanmatunoshita@gmail.com>
2024-05-27 22:06:01 +0000

    navbar

---
+++ my-app/src/app/components/navbar.jsx
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/navigation'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './navbar.module.css';
+
+export default function Navbar({page}){
+    const router = useRouter();
+
+    return(
+        <nav className={styles.navbar}>
+            <ul className={styles.navlist}>
+                <li className={styles.navitem}>
+                    <Link href={"/inicio"}>
+                        <Image
+                            src="/logo_levelup.svg"
+                            width={60}
+                            height={60}
+                        />
+                    </Link>
+                </li>
+                <li className={styles.navitem}>
+                    <input type="text" className={styles.search} placeholder='Pesquisa' />
+                </li>
+                <li className={styles.navitem}>
+                    <Image src={"/carrinho.svg"} 
+                            width={60}
+                            height={60}/>
+                    <Image src={"/perfil (2).svg"}
+                            width={60}
+                            height={60}/>
+                </li>
+            </ul>
+            <ul className={styles.subnavlist}>
+                <li className={(styles.subnavitem, styles.select)}><a href="">Inicio</a></li>
+                <li className={styles.subnavitem}><a href="">Jogos</a></li>
+                <li className={styles.subnavitem}><a href="">Cartões-Presentes</a></li>
+                <li className={styles.subnavitem}><a href="">Xbox</a></li>
+                <li className={styles.subnavitem}><a href="">Playstation</a></li>
+                <li className={styles.subnavitem}><a href="">Nintendo</a></li>
+            </ul>
+
+        </nav>
+    )
+}
--- my-app/src/app/inicio/page.jsx
+++ my-app/src/app/inicio/page.jsx
@@ -1,14 +1,16 @@
 'use client'
 
-import Image from 'next/image';
+
 import styles from './inicio.module.css';
 import { useRouter } from 'next/navigation'
+import Navbar from '../components/navbar';
 
 
 
 export default function Inicio(){
     return(
         <div className={styles.container}>
+            <Navbar/>
             inicio
         </div>
     )
