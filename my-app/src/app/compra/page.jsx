76def36a9b4cd232aec4de1b0745096aac3877aa
Daniel Hideo Matunoshita <dandanmatunoshita@gmail.com>
2024-05-30 16:32:44 +0000

    iinit_compra

--- my-app/src/app/compra/page.jsx
+++ my-app/src/app/compra/page.jsx
@@ -1,9 +1,44 @@
+'use client'
+
+import Navbar from '../components/navbar';
 import styles from './compra.module.css';
 
 export default function Compra(){
     return(
-        <div>
-            Compra
+        <div className={styles.compra_container}>
+            <Navbar/>
+            <div className={styles.compra_body}>
+                <div className={styles.compra_info}>
+                    <h1>Checkout</h1>
+                    <h2>Pagamento</h2>
+                    <form action="">
+                        <div className={styles.compra_nome}>
+                            <label>Nome Completo</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div className={styles.compra_endereco}>
+                            <label>Endereço</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div className={styles.compra_cidade}>
+                            <label>Cidade</label>
+                            <input type="text" id="" />
+                            <label>Estado</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div className={styles.compra_cartao}>
+                            <label>Numero do cartão</label>
+                            <input type="text" id="" />
+                        </div>
+                        <div className={styles.compra_validade}>
+                            <label>Validade</label>
+                            <input type="text" id="" />
+                            <label>CVC</label>
+                            <input type="text" id="" />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
